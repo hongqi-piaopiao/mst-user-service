8ffc9c86af666ebb26b4afc98630ef2159d09c6c
Fan Mengtian <mengtian.fan@gmail.com>
2018-01-15 20:39:24 +0800

    [tt] add environment variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,8 @@ IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
 
 sed -i "s#<IMAGE_NAME>#$IMAGE_NAME#g" docker-compose.yml
 
+export RANCHER_URL=http://10.202.129.239:8080/v2-beta/projects/1a5
+export RANCHER_ACCESS_KEY=B3B549F4A5D7B0B7A6B1
+export RANCHER_SECRET_KEY=n2GMALmnutCu2vLcHRSn7XS9FD2Au9QtEHNycCas
+
 rancher-compose -p mst-user-service up -d -c --upgrade
