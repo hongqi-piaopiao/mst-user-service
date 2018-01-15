50b8b14da25d095abebe59e1be675debdfedeeaa
Fan Mengtian <mengtian.fan@gmail.com>
2018-01-15 20:19:06 +0800

    [tt] #N/A change docker repo address; deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,10 @@ set -x
 set -e
 
 if [[ -z $DOCKER_REGISRTY ]]; then
-  DOCKER_REGISRTY=127.0.0.1:5000
+  DOCKER_REGISRTY=10.202.129.239:5000
 fi
 IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
 
-docker run -d -p 8080:8080 $IMAGE_NAME
+sed -i "s#<IMAGE_NAME>#$IMAGE_NAME#g" docker-compose.yml
+
+rancher-compose -p mst-user-service up -d -c --upgrade
